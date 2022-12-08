8f086f1ae0018aa6b443652917bf9631176293a6
Timur Svoboda <svobodatimur@gmail.com>
2022-12-08 07:35:14 +0200

    refactor: remove pixelPerfect type

--- src/types.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Type for the story's `pixelPerfect` parameter
- */
-export interface PixelPerfectParameter {
-  /**
-   * URL or local path to component overlay image
-   */
-  overlaySrc: string;
-}
