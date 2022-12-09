5f21c35377d016dbbb5eaf9f0df1708f510fcb9f
Timur Svoboda <svobodatimur@gmail.com>
2022-12-09 11:30:52 +0200

    refactor: add better names for the tool

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
 export const ADDON_ID = "pixel-perfect-storybook-addon";
-export const TOOL_ID = `${ADDON_ID}/tool`;
+export const TOGGLE_OVERLAY_TOOL_ID = `${ADDON_ID}/toggle-overlay-tool`;
 export const PARAM_KEY = `pixelPerfect`;
 
--- src/preset/manager.ts
+++ src/preset/manager.ts
@@ -1,15 +1,15 @@
 import { addons, types } from "@storybook/addons";
 
-import { ADDON_ID, TOOL_ID } from "../constants";
-import { Tool } from "../Tool";
+import { ADDON_ID, TOGGLE_OVERLAY_TOOL_ID } from "../constants";
+import { ToggleOverlayTool } from "../ToggleOverlayTool";
 
 // Register the addon
 addons.register(ADDON_ID, () => {
   // Register the tool
-  addons.add(TOOL_ID, {
+  addons.add(TOGGLE_OVERLAY_TOOL_ID, {
     type: types.TOOL,
-    title: "My addon",
+    title: "Toggle overlay",
     match: ({ viewMode }) => !!(viewMode && viewMode.match(/^story$/)),
-    render: Tool,
+    render: ToggleOverlayTool,
   });
 });
