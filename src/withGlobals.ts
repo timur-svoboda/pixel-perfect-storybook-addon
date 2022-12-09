19a43b7f87672ca8146645ba9ea6f32b13f70665
Timur Svoboda <svobodatimur@gmail.com>
2022-12-09 11:06:15 +0200

    fix: remove demo code

--- src/withGlobals.ts
+++ src/withGlobals.ts
@@ -3,24 +3,7 @@ import { useEffect, useGlobals } from "@storybook/addons";
 import renderOverlay from './utils/render-overlay';
 
 export const withGlobals: DecoratorFunction = (StoryFn, context) => {
-  const [{ myAddon, pixelPerfect }] = useGlobals();
-  // Is the addon being used in the docs panel
-  const isInDocs = context.viewMode === "docs";
-  const { theme } = context.globals;
-
-  useEffect(() => {
-    // Execute your side effect here
-    // For example, to manipulate the contents of the preview
-    const selectorId = isInDocs
-      ? `#anchor--${context.id} .docs-story`
-      : `#root`;
-
-    displayToolState(selectorId, {
-      myAddon,
-      isInDocs,
-      theme,
-    });
-  }, [myAddon, theme]);
+  const [{ pixelPerfect }] = useGlobals();
 
   useEffect(() => {
     if (
@@ -39,24 +22,3 @@ export const withGlobals: DecoratorFunction = (StoryFn, context) => {
 
   return StoryFn();
 };
-
-function displayToolState(selector: string, state: any) {
-  const rootElement = document.querySelector(selector);
-  let preElement = rootElement.querySelector("pre");
-
-  if (!preElement) {
-    preElement = document.createElement("pre");
-    preElement.style.setProperty("margin-top", "2rem");
-    preElement.style.setProperty("padding", "1rem");
-    preElement.style.setProperty("background-color", "#eee");
-    preElement.style.setProperty("border-radius", "3px");
-    preElement.style.setProperty("max-width", "600px");
-    rootElement.appendChild(preElement);
-  }
-
-  preElement.innerText = `This snippet is injected by the withGlobals decorator.
-It updates as the user interacts with the ⚡ or Theme tools in the toolbar above.
-
-${JSON.stringify(state, null, 2)}
-`;
-}
